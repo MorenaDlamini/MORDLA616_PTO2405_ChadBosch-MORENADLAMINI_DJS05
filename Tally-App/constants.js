51c8e89d181eae1beab95a65b26154c173420e4d
MorenaDlamini <dlaminimorena@gmail.com>
2025-02-16 19:47:51 +0200

    Added Constant file

---
+++ Tally-App/constants.js
@@ -0,0 +1,14 @@
+/**
+ * Action type constants for the tally application.
+ * 
+ * @module constants
+ */
+
+/** @constant {string} ADD - Action type for adding to the counter. */
+export const ADD = 'ADD';
+
+/** @constant {string} SUBTRACT - Action type for subtracting from the counter. */
+export const SUBTRACT = 'SUBTRACT';
+
+/** @constant {string} RESET - Action type for resetting the counter. */
+export const RESET = 'RESET';
